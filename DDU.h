13388269ac75f5087f1df7bb40c7bb475fd846b8
Simon Dischl <simon.dischl@shw.de>
2023-11-08 12:40:44 +0100

    implemented functions, structs

--- DDU.h
+++ DDU.h
@@ -1,14 +1,20 @@
 
 // color definitions
-#define WHITE   {255, 255, 255}
-#define GRAY    {150, 150, 150}
-#define YELLOW  {250, 240, 0}
-#define ORANGE  {255, 130, 0}
-#define RED     {240, 0, 0}
-#define PURPLE  {180, 0, 220}
-#define CYAN    {0, 190, 255}
-#define BLACK   {0, 0, 0}
+// TODO convert colors to hex values
+#define DDU_WHITE   {255, 255, 255}
+#define DDU_GRAY    {150, 150, 150}
+#define DDU_YELLOW  {250, 240, 0}
+#define DDU_ORANGE  {255, 130, 0}
+#define DDU_RED     {240, 0, 0}
+#define DDU_PURPLE  {180, 0, 220}
+#define DDU_CYAN    {0, 190, 255}
+#define DDU_BLACK   {0, 0, 0}
+#define DDU_GREEN   {0, 240, 0}
+
+#define DDU_BACKGROUND DDU_BLACK
 
 // display resolution
-#define WIDTH   480
-#define HEIGHT  272
+#define DDU_WIDTH   480
+#define DDU_HEIGHT  272
+
+#define DDU_REFRESH_DELAY 5000
--- DDU.ino
+++ DDU.ino
@@ -30,9 +30,14 @@ Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
 
 // function prototypes
 void generateData();
+void refreshDisplay();
+void drawFrame(struct frame);
+void drawGear(char);
+void drawPageIndicator();
 
 
-struct{
+// holds all the data
+struct data{
     unsigned int tOil;
     unsigned int tFuel;
     unsigned int tMot;
@@ -47,44 +52,152 @@ struct{
     float lambda;
     float uBatt;
     unsigned int tBatt;
-}data;
+};
 
-unsigned long refreshTimer = 0;
+// holds number of values inside one frame
+struct frame{
+    unsigned int numOfVals;
+    char* title;
+    unsigned int color;
+    unsigned int posX;
+    unsigned int posY;
+    unsigned int sizeX;
+    unsigned int sizeY;
+    struct value values[3];
+};
 
+// holds a single name-value pair
+struct value{
+    char* name;
+    char* value;
+};
+
+// holds one value in its own frame
+struct valueFrame{
+    char* title;
+    char* value;
+    unsigned int color;
+    unsigned int posX;
+    unsigned int posY;
+    unsigned int sizeX;
+    unsigned int sizeY;
+};
+
+struct frame temp;
+struct frame pres;
+struct valueFrame rpm;
+struct valueFrame ath;
+struct valueFrame map;
+struct valueFrame tc;
+struct valueFrame mode;
+struct valueFrame lam;
+struct frame batt;
 
 
 
 void setup(void)
 {
+    // init screen and draw background
     gfx->begin();
-    gfx->fillScreen(BLACK);
+    gfx->fillScreen(DDU_BACKGROUND);
 
-#ifdef TFT_BL
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH);
-#endif
+    // enable backlight
+    // pinMode(TFT_BL, OUTPUT);
+    // digitalWrite(TFT_BL, HIGH);
 
+    // DEBUG write Hello World
     gfx->setCursor(10, 10);
     gfx->setTextColor(RED);
+    gfx->setTextSize(random(6) /* x scale */, random(6) /* y scale */, random(2) /* pixel_margin */);
     gfx->println("Hello World!");
 
-    delay(5000);
+    delay(100);
 }
 
 void loop()
 {
-    gfx->setCursor(random(gfx->width()), random(gfx->height()));
-    gfx->setTextColor(random(0xffff), random(0xffff));
-    gfx->setTextSize(random(6) /* x scale */, random(6) /* y scale */, random(2) /* pixel_margin */);
-    gfx->println("Hello World!");
+    generateData();
 
-    delay(1000);
+    refreshDisplay();
+
+    delay(DDU_REFRESH_DELAY);
 }
 
+// fill frame and valueFrame structs with values that don't change constantly
+void initStructs()
+{
+    struct value nullvalue = {"NULL", "0"};
 
+    // temps struct
+    temp.numOfVals = 3;
+    temp.title = "TEMP";
+    temp.color = DDU_ORANGE;
+    temp.posX = 1;
+    temp.posY = 1;
+    temp.sizeX = 1;
+    temp.sizeY = 1;
+    struct value tFuel = {"FUEL", 0};
+    struct value tMot = {"MOT", 0};
+    struct value tOil = {"OIL", 0};
+    temp.values[0] = tOil;
+    temp.values[1] = tFuel;
+    temp.values[2] = tMot;
+
+    // pressures struct
+    pres.numOfVals = 2;
+    pres.title = "PRES";
+    pres.color = DDU_CYAN;
+    pres.posX = 1;
+    pres.posY = 1;
+    pres.sizeX = 1;
+    pres.sizeY = 1;
+    struct value pOil = {"OIL", 0};
+    struct value pFuel = {"FUEL", 0};
+    pres.values[0] = pOil;
+    pres.values[1] = pFuel;
+    pres.values[2] = nullvalue;
+
+    // rpm struct
+    rpm = {"RPM", 0, DDU_WHITE, 1, 1, 1, 1};
+
+    // ath struct
+    ath = {"ATH", 0, DDU_WHITE, 1, 1, 1, 1};
+
+    // map struct
+    map = {"MAP", 0, DDU_YELLOW, 1, 1, 1, 1};
+
+    // tc struct
+    tc = {"TC", 0, DDU_RED, 1, 1, 1, 1};
+
+    // mode struct
+    mode = {"MODE", 0, DDU_WHITE, 1, 1, 1, 1};
+
+    // lambda struct
+    lam = {"LAM", 0, DDU_PURPLE, 1, 1, 1, 1};
+
+    // battery struct
+    batt.numOfVals = 2;
+    batt.title = "BATT";
+    batt.color = DDU_GREEN;
+    batt.posX = 1;
+    batt.posY = 1;
+    batt.sizeX = 1;
+    batt.sizeY = 1;
+    struct value uBatt = {"VOLT", 0};
+    struct value tBatt = {"TEMP", 0};
+    batt.values[0] = uBatt;
+    batt.values[1] = tBatt;
+    batt.values[2] = nullvalue;
+}
+
+// generates random values, updates data struct
 void generateData()
 {
     // TODO Eastereggs einbauen
+
+    // calculate random data
+    struct data data;
+
     unsigned int tEnv = random(15, 35);
 
     data.tFuel = min(tEnv + random(0, 15), 40);
@@ -135,4 +248,51 @@ void generateData()
     data.lambda = random(9, 11) / 10.0f;
     data.uBatt = random(132, 168) / 10.0f;
     data.tBatt = min(tEnv + random(0, 15), 39);
+
+
+    // fill frame structs
+    temp.values[0].value = sprintf("%d", data.tMot);
+    temp.values[1].value = sprintf("%d", data.tFuel);
+    temp.values[2].value = sprintf("%d", data.tMot);
+
+    pres.values[0].value = sprintf("%0.02f", data.pOil);
+    pres.values[1].value = sprintf("%0.02f", data.pFuel);
+
+    batt.values[0] = sprintf("%00.00f", data.uBatt);
+    batt.values[1] = sprintf("%d", data.tBatt);
+
+    // fill single value structs
+    rpm.value = sprintf("%d", data.n);
+    ath.value = sprintf("%00.0f", data.ath);
+    map.value = sprintf("%d", data.map);
+    tc.value = sprintf("%d", data.tc);
+    mode.value = data.mode;
+    lam.value = sprintf("%0.00f", data.lambda);
+}
+
+// writes the values in data struct to their places on the screen
+void refreshDisplay()
+{
+    // TODO construct screen, draw Frames
+}
+
+void drawFrame(struct frame frame)
+{
+    // TODO draw the frame, title and contained values
+}
+
+
+void drawGear(char c)
+{
+    const int width = 40;
+    const int height = 80;
+    const int posX = gfx->width()/2 - width/2;
+    const int posY = gfx->height()/2 - height/2;
+    
+    // TODO draw gear character
+}
+
+void drawPageIndicator()
+{
+    // TODO draw three circles as page indicators, left one is WHITE, rest is GREY
 }

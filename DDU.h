e90d5de487b877b59defae93373911d7670414e9
Simon Dischl <simon.dischl@yahoo.de>
2023-11-09 21:13:26 +0100

    added RWU colors

--- DDU.h
+++ DDU.h
@@ -4,13 +4,15 @@
 // color definitions
 #define DDU_WHITE   0xffff      // {255, 255, 255}
 #define DDU_GRAY    0x94b2      // {150, 150, 150}
-#define DDU_YELLOW  0xff80      // {250, 240, 0}
-#define DDU_ORANGE  0xfc00      // {255, 130, 0}
-#define DDU_RED     0xf000      // {240, 0, 0}
-#define DDU_PURPLE  0xb01b      // {180, 0, 220}
-#define DDU_CYAN    0x05ff      // {0, 190, 255}
-#define DDU_BLACK   0x0000      // {0, 0, 0}
-#define DDU_GREEN   0x0780      // {0, 240, 0}
+#define DDU_YELLOW  0xff80      // {250, 240, 000}
+#define DDU_ORANGE  0xfc00      // {255, 130, 000}
+#define DDU_RED     0xf000      // {240, 000, 000}
+#define DDU_PURPLE  0xb01b      // {180, 000, 220}
+#define DDU_CYAN    0x05ff      // {000, 190, 255}
+#define DDU_BLACK   0x0000      // {000, 000, 000}
+#define DDU_GREEN   0x0780      // {000, 240, 000}
+#define RWU_PURPLE  0x61D6      // {102, 056, 182}
+#define RWU_CYAN    0x1369      // {000, 169, 206}
 
 #define DDU_BACKGROUND DDU_BLACK
 
